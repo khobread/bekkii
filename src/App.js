b1f2b09a11dbc62827bec38e5bdb3da09b8daa44
Kevin <>
2021-01-25 17:28:52 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap';
+//stick to div's
 
 function App() {
   return (
     <div className="App">
-      
-      <header className="App-header">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">BEKKII</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Catalog</Nav.Link>
+            <Nav.Link href="#link"></Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">BEKKII</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Sarch</Button>
+          </Form>
+        </Navbar.Collapse>
 
-        <div className="navbar">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Order</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
+      </Navbar>
 
-        <h1>Bekkii</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
-
-
-      <div>
-
-      </div>
-
-
-      <footer>
-
-      </footer>
+      <Container fluid>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
--- src/tests/app.spec.js
+++ src/tests/app.spec.js
@@ -5,11 +5,24 @@ import {render, screen} from "@testing-library/react"
 describe("App", () => {
     it("has a header that says Bekkii", () => {
         render(<App />);
-        expect(screen.getByText("Bekkii")).toBeInTheDocument();
+        expect(screen.getByRole('link', {name: "BEKKII", href: /#home/i})).toBeInTheDocument();
     });
+    it("has a Search button", () => {
+        render(<App />);
+        expect(screen.getByRole('button', {name: "Search"})).toBeInTheDocument();
+    });
+    // it("has a header that says Bekkii", () => {
+    //     render(<App />);
+    //     expect(screen.getByRole('link', {name: "BEKKII", href: /#home/i})).toBeInTheDocument();
+    // });
+    // it("has a header that says Bekkii", () => {
+    //     render(<App />);
+    //     expect(screen.getByRole('link', {name: "BEKKII", href: /#home/i})).toBeInTheDocument();
+    // });
     
 }); //arrow functions > functions for callback (ES2015; best practice)
 
 //red/green = just make the test pass; ignore best practice
 //refactoring: go back and organize code according to best practice
 
+//be as specific as you can when testing.
